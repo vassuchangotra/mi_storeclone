665fc2b69d7c75fa82a9e2749d5e14bea75391e1
vassuchangotra <vassuchangotravC@gmail.com>
2022-04-14 22:38:54 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Navbar from './Navbar.js';
+
+function App() {
+  return (
+
+    <div >
+     Helllo
+     <h1>
+       yo
+<Navbar />
+     </h1>
+    </div>
+  );
+}
+
+export default App;
